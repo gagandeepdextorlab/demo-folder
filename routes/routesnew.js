122360ead5d6f72a3a84a8da70bd4d1538fcd4e1
Mohit <kumavatmohi@gmail.com>
2026-09-15 13:51:52 +0530

    routes new

--- routes/routesnew.js
+++ routes/routesnew.js
@@ -4,11 +4,11 @@ import { getAllUsers, createUser , loginUser, getUserById , updateUser, deleteUs
 const router = Router();
 
 
-router.get("/api/users", getAllUsers);
-router.post("/api/users", createUser);
 router.get("/api/users/:email", getUserById);
+router.post("/api/users", createUser);
 router.put("/api/users/:email", updateUser);
-router.delete("/api/users/:email", deleteUser)
+router.get("/api/users", getAllUsers);
 router.post("/api/users/login", loginUser);
+router.delete("/api/users/:email", deleteUser)
 
 export default router;
